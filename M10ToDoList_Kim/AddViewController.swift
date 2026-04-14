d3b425b6f2173e48b405550719197d409fd01363
Dustin <redhotblood1010@gmail.com>
2026-04-14 14:12:28 -0400

    initial after learning what it does

---
+++ M10ToDoList_Kim/AddViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddViewController.swift
+//  M10ToDoList_Kim
+//
+//  Created by Eun Mi Kim on 4/14/26.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
